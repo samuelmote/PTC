e057b756e4a47706ba0d8523a41f99fc228519d5
samuel mote <sammote06@gmail.com>
2023-03-30 19:20:57 +0000

    (answer componet)

---
+++ src/routes/componets /answers.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let answer = false;
+ </script>
+ {#if answer}
+ <p>
+    Great job - that was the answer!
+ </p>
+ {/if}
+ <script>
+    export let answer = true;
+ </script>
+ {#if answer}
+ <p>
+    Oh man, that answer wasn't quite right...
+ </p>
+ {/if}
