f74c97692f5ecd331dba66010c5d58a088f51acf
samuel mote <sammote06@gmail.com>
2023-03-30 19:23:56 +0000

    wrong and right colors

---
+++ src/components/answers.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let answer= true;
+ </script>
+ {#if answer}
+ <p>Great job</p>
+    {:else} 
+    <p> wasn't quite right...</p>
+ {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,17 @@
 <script>
-	import Question from "./componets /question.svelte";
-import Score from "./componets /score.svelte";
-
+	import Question from '../components/question.svelte';
+	import Score from '../components/score.svelte';
 </script>
 
-<Score  score= {14}/>
+<Score score={0} />
 
-<Question 
-number={1}
-    answer1= "first answer"
-    answer2= "second answer"
-    answer3= "third answer"
-    answer4= "None of the above"
-This is my Question?
+<Question
+	number={1}
+	answer1="first answer"
+	answer2="second answer"
+	answer3="third answer"
+	answer4="fourth answer"
+    answer="a"
 >
+	This is my Question?
 </Question>
-
-
