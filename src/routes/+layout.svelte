1788ea31ddc72b79724084ec2d019ef7680df8ec
samuel mote <sammote06@gmail.com>
2023-04-19 15:15:41 +0000

    copied repository

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,72 @@
+
 <script>
+	import Clock from '../components/clock.svelte';
+	import Score from '../components/score.svelte';
+</script>
+
+<div class="background">
+    <div class="layout">
+        <slot />
+    </div>
+	<div class="layout">
+		<div class="header">
+			<Score score={0} />
+			<Clock />
+		</div>
+		<div class="content">
+			<slot />
+		</div>
+	</div>
+</div>
+
+<style>
+    .background {
+        padding: 32px;
+	
+		padding: 32px;
+		display: grid;
+		position: absolute;
+		inset: 0;
+		background-color: var(--color-purple);
+	}
+	.layout {
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		display: grid;
+		grid-template-rows: auto 1fr;
+		align-items: center;
+	}
+        position: absolute;
+        inset: 0;
+	.content,
+	.header {
+		background-color: white;
+		border: 4px solid black;
+		padding: 12px 24px;
+	}
+        background-color: #abacc2;
+    }
+	.header {
+		display: flex;
+		justify-content: space-between;
+	}
+    .layout {
+        width: 100%;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
+	.content {
+		width: 100%;
+		max-width: 1024px;
+	}
+
+
+
+
+
+</style>
 	import Score from '../components/score.svelte';
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
 <script>
+	import Score from "../components/score.svelte";
+	import Question from "../components/question.svelte";
+	
+</script>
+
+<Score score={0} />
+
+<Question
+	number={1}
+	answer1="First answer"
+	answer2="Second answer"
+	answer3="Third answer"
+	answer4="Fourth answer"
+	answer="c"
+>
+<Question number={1} answers={['Yes', 'No', 'Maybe, but not right now']} correctIndex={0}>
+	This is my new question?
+</Question>
+</Question>
+
+
+<script>
 	import Question from '../components/question.svelte';
 	import Score from '../components/score.svelte';
 </script>
